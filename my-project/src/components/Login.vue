ca28e9bf2d9485f16252301b7943af2da3d43b9c
VEARTH <earth14828@gmail.com>
2018-08-11 23:40:23 +0700

    แต่งหน้าlogin

--- my-project/src/components/Login.vue
+++ my-project/src/components/Login.vue
@@ -1,95 +1,77 @@
-<template>
-    
-    <div>
-        
-
-            <b-row class="justify-content-md-center">
-                <b-col sm="2" size="sm">E-MAIL : </b-col >
-                <b-col sm="5" >           
-                    <b-input v-model="form.email" size="sm" ></b-input>
+<template>   
+    <div class="justify-content-md-center" style="margin:5% 20% 5% 20%">
+        <b-card title="กรุณาลงชื่อเข้าใช้" class="text-center">
+            <hr class="my-4">
+            <b-alert variant="danger" dismissible :show="showAlert" @dismissed="showAlert=false">
+                อีเมลล์หรือรหัสผ่านไม่ถูกต้อง
+            </b-alert>    
+            <b-row align-v="center" class="p-2">
+                <b-col sm="3">อีเมลล์ : </b-col >
+                <b-col>           
+                    <b-input v-model="form.email" size="md" type="email" placeholder="xxxxx@example.com"></b-input>
                 </b-col >
             </b-row>
-            <b-row class="justify-content-md-center">
-                <b-col sm="2" size="sm">PASSWORD : </b-col >
-                <b-col sm="5" >           
-                    <b-input v-model="form.password" size="sm" type="password" ></b-input>
+
+            <b-row align-v="center" class="p-2">
+                <b-col sm="3">รหัสผ่าน : </b-col >
+                <b-col>           
+                    <b-input v-model="form.password" size="md" type="password" placeholder="**********"></b-input>
                 </b-col >
             </b-row>
-            <b-row class="justify-content-md-center">
-                <b-button v-on:click ="Login" >Login </b-button>        
-            </b-row>
-
-            <b-modal ref="myModalRef" hide-footer title="Warning">
-                <div class="d-block text-center">
-                    <h3>Wrong e-mail or password</h3>
-                </div>
-                <b-row>
-                    <b-col><b-btn class="mt-3" variant="outline-danger" block @click="hideModal">OK</b-btn></b-col>
-                </b-row>
-                        
-                        
-            </b-modal>
-            
-            
-        
+            <b-button v-on:click ="Login" variant="primary">ลงชื่อเข้าใช้</b-button>
+        </b-card>   
     </div>
 </template>
 <script>
-import store from '../vuex/store.js'
-import Vuex from 'vuex'
-global.vuex = Vuex
+import store from "../vuex/store.js";
+import Vuex from "vuex";
+global.vuex = Vuex;
 
 export default {
-    store,
-    data () {
-    
-        return {
-            form: {
-                name:'',
-                email:'',
-                password:''     
-            },
-            check:0,
-            testemail:'mook@mahidol',  //สมมติเป็นข้อมูลdatabase 
-            testpassword:'mmm', // backend สร้างfunctionเพิ่ม
-            testusername:'mook'
-      
-    }
+  store,
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: ""
+      },
+      showAlert: false,
+      check: 0,
+      testemail: "mook@mahidol", //สมมติเป็นข้อมูลdatabase
+      testpassword: "mmm", // backend สร้างfunctionเพิ่ม
+      testusername: "mook"
+    };
   },
   methods: {
-        Login(){
-        
-            if(this.form.email===this.testemail && this.form.password===this.testpassword)
-            {
-                this.check=1
-                this.form.name=this.testusername
-                this.addName()
-                this.addChange ()
-                
-                this.$router.push('/allshown') //ถ้า username กับ password ผ่านไปหน้ารวม(ของเอิทธ์)
-            }
-            else
-            {
-                this.$refs.myModalRef.show()
-                this.form.name=''
-                this.form.password=''
-            }
-          
-          
-        },
-        showModal () {
-            this.$refs.myModalRef.show()
-        },
-        hideModal () {
-            this.$refs.myModalRef.hide()
-        },
-        addName () {
-            store.commit('addName',this.form.name)
-        },
-        addChange () {
-            store.commit('addChange',this.check)
-        }
-      
+    Login() {
+      if (
+        this.form.email === this.testemail &&
+        this.form.password === this.testpassword
+      ) {
+        this.check = 1;
+        this.form.name = this.testusername;
+        this.addName();
+        this.addChange();
+        this.$router.push("/allshown"); //ถ้า username กับ password ผ่านไปหน้ารวม(ของเอิทธ์)
+      } else {
+        this.showAlert = true;
+        this.form.email = "";
+        this.form.password = "";
+      }
+    },
+    showModal() {
+      this.$refs.myModalRef.show();
+    },
+    hideModal() {
+      this.$refs.myModalRef.hide();
+    },
+    addName() {
+      store.commit("addName", this.form.name);
+    },
+    addChange() {
+      store.commit("addChange", this.check);
+    }
   }
-}
+};
 </script>
