fa8f4321ae91b531088ecff005374e0b2f74800e
sataporn <sataporn.san@student.mahidol.ac.th>
2018-08-14 20:52:12 +0700

    รวมหน้าGraph อยู่ใน App.vue
    ใช้ linechart.vue

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -17,44 +17,81 @@
               <input type="radio" id="var" value="bar" v-model="type_chart" v-on:change="add_all" >
                 <label for="bar">Bar</label>
         </b-col>
-    </b-row> 
+   </b-row> 
    <b-row>
-          <b-col><fil/></b-col>
+          <b-col>  
+                <div>
+                        <b-badge variant="dark">Date</b-badge>
+                        <Flatpickr :options="DateOptions"  v-model="my_filter.date"/>
+                        <!-- <b-badge variant="dark">Form(Time)</b-badge>
+                        <Flatpickr :options=" FtimeOptions"  v-model="my_filter.Ftime"/>
+                        <b-badge variant="dark">To(Time)</b-badge>
+                        <Flatpickr :options=" TtimeOptions"  v-model="my_filter.Ttime"/> -->
+                        <b-button  variant="success" v-on:click="Search">Search</b-button>
+                </div>      
+          </b-col>
             
     </b-row> 
 </b-container> 
 </div>  
 </template>
 <script>
+import flatPickr from 'vue-flatpickr-component';
 import graph from "./test.vue";
-import fil from "./filter-g.vue";
 import LC from "./LineChart.vue";
 import RangeSlider from 'vue-range-slider'
 import 'vue-range-slider/dist/vue-range-slider.css'
 export default {
-  components: { graph, fil, LC,RangeSlider },
+  components: { graph, LC,RangeSlider,   flatPickr },
   data() {
     return {
-      option: { responsive:false, maintainAspectRatio: false, 
-      //  scales: {
-      //           yAxes: [ {ticks: {min: 0, max:24}}]
-      //          },         
-       },
-      timeline:[],
-      tmp_data:{},
-      labes: [],
-      dataa_uv: { labels: [], datasets: [] },
-      dataa_tmp: { labels: [], datasets: [] },
-      dataa_wind: { labels: [], datasets: [] },
-      dataa_humid: { labels: [], datasets: [] },
-      type_chart:'line',
-      sliderValue: 0,
+                  my_filter:{
+                      date:'',
+                      Ftime:'',
+                      Ttime:''
+                  },
+                  DateOptions: {
+                      // mode: "range",
+                      utc: true,
+                      defaultDate: '2016-12-27T16:16:22.585Z',
+                  },   
+                    FtimeOptions: {
+                        enableTime: true,
+                        noCalendar: true,
+                        time_24hr: true,
+                        defaultDate: "12:00"
+                  }, 
+                  TtimeOptions: {
+                        enableTime: true,
+                        noCalendar: true,
+                        time_24hr: true,
+                        defaultDate: "12:00"
+                  }, 
+                option: { responsive:false, maintainAspectRatio: false, 
+                //  scales: {
+                //           yAxes: [ {ticks: {min: 0, max:12}}]
+                //          },         
+                },
+                timeline:[1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1],
+                tmp_data:[1,2,3,4,5,6,6,5,4,3,2,1,1,2,3,4,5,6,6,5,4,3,2,1,1,2,3,4,5,6,6,5,4,3,2,1,1,2,3,4,5,6,6,5,4,3,2,1],
+                labes: [],
+                dataa_uv: { labels: [], datasets: [] },
+                dataa_tmp: { labels: [], datasets: [] },
+                dataa_wind: { labels: [], datasets: [] },
+                dataa_humid: { labels: [], datasets: [] },
+                type_chart:'line',
+                sliderValue: 0,
     };
   },
   methods: {
+       Search()
+        {
+            console.log(JSON.stringify(this.my_filter));
+            return JSON.stringify(this.my_filter);
+        },
     add_uv(x) {
       this.dataa_uv = {
-        labels:this.timeline.slice(x,x+12),
+        labels:this.timeline.slice(x,x+30),
         datasets: [
           {
             label: "UV",
@@ -63,7 +100,7 @@ export default {
             borderWidth: 1,
             pointBorderColor: "white",
             backgroundColor: this.gradient,
-            data:this.tmp_data.slice(x,x+12),
+            data:this.tmp_data.slice(x,x+30),
             type:this.type_chart
           }
         ]
@@ -129,7 +166,6 @@ export default {
     }, 
     getUnits: function() {this.add_all()}
   },
-  
     beforeMount(){this.getUnits()
  },
 };
